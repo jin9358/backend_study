c28d451005bf5421d5be0fa10b0c514cccbb2471
jin9358 <leejh9358@naver.com>
2025-05-21 09:31:09 +0900

    feat: 20250520 요청데이터, ioc

---
+++ src/main/java/com/korit/springboot/dto/param/SearchStudyDto.java
@@ -0,0 +1,12 @@
+package com.korit.springboot.dto.param;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.lang.NonNull;
+
+@Data
+public class SearchStudyDto {
+    private String searchValue;
+    private Integer page = 1;
+    private Integer count = 10;
+}
