9c35b77ef10fa4fe7a4be348a628275f1a91f349
jin9358 <leejh9358@naver.com>
2025-05-21 17:14:26 +0900

    feat: 20250521 study

---
+++ src/main/java/com/korit/springboot/controller/HttpStudyController4.java
@@ -0,0 +1,26 @@
+package com.korit.springboot.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * REST API
+ * 요청과 응답
+ * https://m.blog.naver.com/codingbarbie/223233477242
+ * https://hahahoho5915.tistory.com/54
+ */
+
+@RestController
+public class HttpStudyController4 {
+
+    @GetMapping("/api/study/http/get")
+    public ResponseEntity<Map<String, Object>> get() {
+//        return new ResponseEntity<>(Map.of("name", "김준일", "age", 32), HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(Map.of("name", "김준일", "age", 32));
+    }
+}
